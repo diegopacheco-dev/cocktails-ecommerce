3a0884d04b7a486a320d25229c1e2cd134f08022
ericksonder <ericksonder017@gmail.com>
2021-09-12 17:05:18 -0500

    Style: añadiendo estilos al componente ProductCard

--- src/components/ProducCard/ProductCard.js
+++ src/components/ProducCard/ProductCard.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./styles.css";
 
-const ProductCard = ({product, action}) => {
-    return (
-        <div>
-            <img src={product.strDrinkThumb} alt="" />
-            <p>{product.strDrink}</p>
-            <p>{product.precio} </p>
-        </div>
-    )
-}
+const ProductCard = ({ product, action }) => {
+  return (
+    <div className="product-card">
+      <div className="product-card__img">
+        <img src={product.strDrinkThumb} alt="" />
+      </div>
 
-export default ProductCard
+      <div className="product-card__info">
+        <h4>{product.strDrink}</h4>
+        <p>S/ {product.precio} </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
